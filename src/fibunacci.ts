864b75cc59e424fe5fed3580b52d45b35ef30abf
Roland Tritsch <roland@tritsch.email>
2025-02-19 16:07:15 +0000

    Prevent stackoverflow.

--- src/fibunacci.ts
+++ src/fibunacci.ts
@@ -1,8 +1,14 @@
+
+// This implementation is recursive and slow and will (sooner or
+// later) create a stack overflow. Means we need to restrict the
+// scope of the `n` variable.
+const recursiveMaxN = 40;
 function recursive(n: number): bigint {
-  if (n === 0) return 0n
-  else if (n === 1) return 1n
-  else if (n >= 2) return recursive(n - 1) + recursive(n - 2)
-  else throw new Error('n must be a non-negative integer')
+  if (n === 0) return 0n;
+  else if (n === 1) return 1n;
+  else if (n >= 2 && n <= recursiveMaxN) return recursive(n - 1) + recursive(n - 2);
+  else if (n > recursiveMaxN) throw new Error(`n must be between 0 and ${recursiveMaxN}`);
+  else throw new Error('n must be a non-negative integer');
 }
 
 export { recursive };
--- tests/fibunacci.test.ts
+++ tests/fibunacci.test.ts
@@ -15,9 +15,14 @@ describe('fib.recursive', () => {
     expect(fib.recursive(4)).toBe(3n);
     expect(fib.recursive(5)).toBe(5n);
     expect(fib.recursive(6)).toBe(8n);
+    expect(fib.recursive(40)).toBe(102334155n);
+  });
+
+  test('should throw error for n > 40', () => {
+    expect(() => fib.recursive(41)).toThrow(/n must be between 0 and 40/);
   });
 
   test('should throw error for negative numbers', () => {
-    expect(() => fib.recursive(-1)).toThrow(/non-negative/);
+    expect(() => fib.recursive(-1)).toThrow(/n must be a non-negative integer/);
   });
 });
